83eaab0b35b644c2986061f38d992ce3918fffad
Deepak <deepak170602@gmail.com>
2023-08-23 19:17:37 +0530

    files added

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from src.config import configuration
-from src.components import data_ingestion
+from src.components import model_trainer
 
 try:
     config = configuration.ConfigurationManager()
-    data_ingestion_config = config.get_data_ingestion_config()
-    data_ingestion = data_ingestion.DataIngestion(config=data_ingestion_config)
-    data_ingestion.download_file()
-    data_ingestion.extract_zip_file()
+    model_trainer_config = config.get_model_trainer_config()
+    model_trainer_config = model_trainer.ModelTrainer(config=model_trainer_config)
+    model_trainer_config.train()
 except Exception as e:
     raise e
